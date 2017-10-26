37831f92583d216b9542cd6f61ee065e5db8f30f
Paul Nadolny <Paul.Nadolny@transplace.com>
2017-10-26 11:26:04 -0500

    prototyping

--- src/main/java/com/transplace/tracking/fourkites/FourkitesTransformerService.java
+++ src/main/java/com/transplace/tracking/fourkites/FourkitesTransformerService.java
@@ -1,16 +1,32 @@
 package com.transplace.tracking.fourkites;
 
 import com.transplace.tracking.fourkites.beans.StopUpdate;
+import com.transplace.tracking.tms.CheckCall;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FourkitesTransformerService {
 
-    public Message<StopUpdate> transform(Message<StopUpdate> msg) {
+    public Message<CheckCall> transform(Message<StopUpdate> msg) {
         msg.getHeaders();
         msg.getPayload();
-        return msg;
+
+        Message<CheckCall> checkCallMessage = new Message<CheckCall>() {
+            @Override
+            public CheckCall getPayload() {
+
+                CheckCall checkCall = new CheckCall();
+                checkCall.setCheckCallType("ARRIVAL");
+                return checkCall;
+            }
+            @Override
+            public MessageHeaders getHeaders() {
+                return msg.getHeaders();
+            }
+        };
+        return checkCallMessage;
     }
 
 }
---
+++ src/main/java/com/transplace/tracking/tms/CheckCall.java
@@ -0,0 +1,14 @@
+package com.transplace.tracking.tms;
+
+public class CheckCall {
+
+    public String getCheckCallType() {
+        return checkCallType;
+    }
+
+    public void setCheckCallType(String checkCallType) {
+        this.checkCallType = checkCallType;
+    }
+
+    private String checkCallType;
+}
