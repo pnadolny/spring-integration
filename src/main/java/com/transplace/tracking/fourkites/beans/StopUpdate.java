60b1c4ab21110bce59a95811911a1372ae23b243
Paul Nadolny <psnadolny@gmail.com>
2017-12-26 11:38:29 -0600

    Update StopUpdate.java

--- src/main/java/com/transplace/tracking/fourkites/beans/StopUpdate.java
+++ src/main/java/com/transplace/tracking/fourkites/beans/StopUpdate.java
@@ -1,8 +1,4 @@
- /*********************** 
- * Copyright © Transplace, Inc, 2012. All rights reserved. 
- * Unauthorized copying or usage of this file, via any medium is strictly prohibited. 
- * Proprietary and confidential. Illegal distribution of files prohibited via any manner.
- **********************/
+ 
 package com.transplace.tracking.fourkites.beans;
 
  import com.fasterxml.jackson.annotation.JsonProperty;
